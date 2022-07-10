842dde02bb112b024285f488156c66d0952528c1
Mert Güney <mert_guney_29@hotmail.com>
2022-07-11 02:25:27 +0300

    TestMethod and Parameter TestMethod

--- XUnitTest.Test/CalculatorTest.cs
+++ XUnitTest.Test/CalculatorTest.cs
@@ -8,7 +8,7 @@ namespace XUnitTest.Test
 {
     public class CalculatorTest
     {
-        [Fact]
+        [Fact]//test metodu olucağını belirtir ve parametre almayan bir metot olduğu belirtilir.
         public void AddTest()
         {
             //Bir test oluşturma 3 aşamadan oluşur
@@ -20,6 +20,17 @@ namespace XUnitTest.Test
             var total = calculator.Add(a, b);
             //Assert (doğrulama evresidir act evresinden çıkan sonucun doğruluğunu test ederiz)
             Assert.Equal(25, total);
+        }
+        [Theory]
+        [InlineData(5, 5, 10)]
+        [InlineData(10, 10, 20)]
+        public void AddTestParams(int a, int b, int ExpectedTotal)
+        {
+            var calculator = new Calculator();
+
+            var actualData = calculator.Add(a, b);
+
+            Assert.Equal(ExpectedTotal, actualData);
         }
     }
 }
