93e221eaa3dcc450001b4ad19a51e0915620fc2e
Mert Güney <mert_guney_29@hotmail.com>
2022-07-12 15:15:30 +0300

    MVC Core project added for test

---
+++ XUnitTest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XUnitTest.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
